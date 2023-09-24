1bb2a6679e0ed05613563dfe7d4d0e89f3c03958
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-09-24 13:29:00 +0400

    moved the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import './styles/base.scss'
 
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/Home";
+import Gallery from "./pages/Gallery/Gallery";
+import Contacts from "./pages/Contacts/Contacts";
+
+function App() {
   return (
-    <>
-      <div>
-        hello world
-      </div>
-    </>
-  )
+    <div className="App">
+      <Router>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
